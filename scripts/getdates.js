9fd52e4155b0e9a3fafe8a77c968e0141d251381
charissehl <hea24004@byui.edu>
2024-04-25 14:28:14 +1200

    included pad with zero js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,17 +1,27 @@
+// to add a leading zero if less than 10
+function padWithZero(number) {
+    return number < 10 ? '0' + number : number;
+}
+
 // select the DOM elements for output
 const medium = document.querySelector("#medium");
 const year = document.querySelector("#year");
 
-
-// use the date object
+// use the date object to get current time
 const today = new Date();
 
-medium.innerHTML = `Last modified: <span class="last-mod">${new Intl.DateTimeFormat(
+// format date and time
+const formatDate = new Intl.DateTimeFormat(
     "en-US",
     {
         dateStyle: "medium"
     }
-).format(today)}, ${today.getHours()}:${today.getMinutes()}</span>`;
+).format(today);
 
+// format time, with leading 0 if necessary
+const formatTime = `${padWithZero(today.getHours())}:${padWithZero(today.getMinutes())}:${padWithZero(today.getSeconds())}`;
+
+// medium and year content
+medium.innerHTML = `Last modified: <span class="last-mod">${formatDate}, ${formatTime}</span>`;
 year.innerHTML = `©${today.getFullYear()} 🐾 Charisse Hearns-Lemon 🐾 Auckland NZ`;
 
